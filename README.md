0d9da1087919d14b9509173eb4b92938457d5021
davinasct4044 <davinascimento4044@gmail.com>
2026-05-15 21:11:40 -0300

    Foi implantado: funcionalidade nas rotas, importação de modules, uso do request para pegar o content do usuario, conversão e utilização do JSON gerado pela IA, uso do module session para armazenar o questionario, correção de prompt para IA e correção do README.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # QuestLab
-Um projeto com o objetivo de aprimorar o aprendizado de estudantes, recebendo o conteúdo
+Um projeto com o objetivo de aprimorar o aprendizado de estudantes, recebendo o conteúdo pelo aluno e gerando uma prova com IA para testar seus conhecimentos.
--- app.py
+++ app.py
@@ -1,18 +1,29 @@
-from flask import Flask #importa a lib Flask
+from flask import Flask, url_for, render_template, request, redirect, session #importa a lib Flask
 from questgeneratorAI import create_exam #importa o modulo que cria o questionario
+import os
+from dotenv import load_dotenv
+from json import load, loads
 
 app = Flask(__name__)
 
+load_dotenv()
+app.secret_key = os.getenv('SESSION_KEY') 
 
 @app.route('/') #tela principal onde o usuário digita o texto para a IA
-def render_input():
-    print()
+def render_index():
+    return render_template("index.html")
 @app.route('/generating_exam' , methods=['POST'])#IA gera as questões → redireciona pra /exam
 def generate_exam():
-    print()
+    contentUser = request.form['inputcontent']
+    quest_generated = create_exam(contentUser)
+
+    session['quest_generated'] = quest_generated
+    return redirect(url_for('render_exam'))
+
 @app.route('/exam' , methods=['GET'])#exibe o questionário
-def hello_world():
-    print()
+def render_exam():
+    quest = session.get('quest_generated')
+    return render_template('exam.html', questoes=quest)
 
 @app.route('/result', methods=['GET']) #mostra quantas acertou
 def result():
